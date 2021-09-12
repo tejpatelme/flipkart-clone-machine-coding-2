12bb6531a45b8bba0ae912e6d1a5a9c55f1f917d
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-09-12 12:32:08 +0530

    functionality complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import Nav from "./components/Nav/Nav";
+import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello Vite + React + Tailwindcss + Eslint</p>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import ProductsProvider from "./contexts/products-context";
+import { BrowserRouter as Router } from "react-router-dom";
+import CartProvider from "./contexts/cart-context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ProductsProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ProductsProvider>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
